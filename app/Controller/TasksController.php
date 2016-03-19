161d1f4b54903ac5b238c42401b952aff88e1be0
shun <shungiku99@gmail.com>
2016-03-19 18:05:13 +0900

    タスク完了

--- app/Controller/TasksController.php
+++ app/Controller/TasksController.php
@@ -4,6 +4,9 @@ class TasksController extends AppController {
     // public $scaffold;//scaffoldはコメントアウト
 
     public $helpers = array('Html','Form');
+
+    public $components = array('Flash');
+
     public function index()
     {
         $options = array(
@@ -15,6 +18,21 @@ class TasksController extends AppController {
         $tasks = $this->Task->find('all' , $options);
 
         $this->set('tasks', $tasks);
+    }
+
+    public function done($id)
+    {
+        //URL -> /tasks/done/5 => $id は 5
+        $this->Task->id = $id;
+        //単一のレコードのカラムを更新
+        $this->Task->saveField('status', 1);
+
+        $msg = sprintf('タスク %s を完了しました' ,$id);
+        //完了したことをフラッシュメッセージで表示
+        $this->Flash->success($msg);
+
+    return $this->redirect(array('action' => 'index'));
+
     }
 
 }
